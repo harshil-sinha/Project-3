8bd8064ba7d29b6ecf191247bc3bab1ee7ad3b5c
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-09 14:17:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+import AddUser from "./components/Users/AddUser.js";
+import UsersList from "./components/Users/UsersList.js";
+function App() {
+  const [usersList,setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge)=>{
+    setUsersList((prevUsersList)=>{
+      return [...prevUsersList,{name : uName, age:uAge, id:Math.random().toString()}];
+    });
+  }
+  return (
+   <>
+  <AddUser onAddUser ={addUserHandler}/>
+  <UsersList users={usersList}/>
+   </>
+  );
+}
+
+export default App;
